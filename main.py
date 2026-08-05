2f72ad8f3467b7692d282a981f6883074413ffed
Beatriz <beatrizmartinsantunes@gmail.com>
2026-08-05 08:51:38 -0300

    inicio sistema de cadastro de livro

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import os
 print("Sistema de uma biblioteca")
+def limpa():
+    os.system('cls')
+class Biblioteca:
+    def __init__(self):
+        self.livros = []
+
+def cadastro_de_livros():
+    print("   CADRASTRO DE LIVROS    ")
+    titulo=str(input("Nome do Livro: "))
+    autor=str(input("Autor do Livro: "))
+    ano_publicado=int(input("Ano de publicação: "))
+    codigo=str(input("Código do Livro: "))
+    status=bool(input("Status do Livro (Disponível/Indisponível): "))
+
+
+
